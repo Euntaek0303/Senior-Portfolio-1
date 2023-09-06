083a8bf56561f351bac4f269969c361fc3bba332
Euntaek Kim <Ki2543Eu0303@pusd.us>
2023-09-06 10:52:17 -0700

    Msg

--- project.py
+++ project.py
@@ -1,1 +1,9 @@
-print("hello")
+print("Welcome to the Space Travel Calculator")
+
+distance = input("Hello, please enter the distance it takes to reach the your destination in light years")
+
+speed = input("Please enter the speed of your spacecraft (in fraction of the speed of light)")
+
+time = int(distance)/int(speed)
+
+print("It will take you", time , "years to reach your destination")
